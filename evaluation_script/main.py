51f02706c5f02d05c6b38b4b88879e5efa5e86d7
NSavov <nedko066@gmail.com>
2024-08-08 21:45:36 +0000

    eval fix

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -40,42 +40,26 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
         }
     """
     output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
+    print("Evaluating for Test Phase")
+    output["result"] = [
+        {
+            "train_split": {
+                "ML": random.randint(0, 99),
+                "NLP": random.randint(0, 99),
+                "CV": random.randint(0, 99),
+                "Total": random.randint(0, 99),
             }
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
-        print("Evaluating for Test Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-            {
-                "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]
-        print("Completed evaluation for Test Phase")
+        },
+        {
+            "test_split": {
+                "ML": random.randint(0, 99),
+                "NLP": random.randint(0, 99),
+                "CV": random.randint(0, 99),
+                "Total": random.randint(0, 99),
+            }
+        },
+    ]
+    # To display the results in the result file
+    output["submission_result"] = output["result"][0]
+    print("Completed evaluation for Test Phase")
     return output
