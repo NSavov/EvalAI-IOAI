60d7288216e64716c834e437ac06073936094c1a
NSavov <nedko066@gmail.com>
2024-08-09 12:26:58 +0000

    no modal

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -4,37 +4,37 @@
 # A. Uncomment the below code to install the custom python packages.
 """
 
-import os
-import subprocess
-import sys
-from pathlib import Path
+# import os
+# import subprocess
+# import sys
+# from pathlib import Path
 
-def install(package):
-    # Install a pip python package
+# def install(package):
+#     # Install a pip python package
 
-    # Args:
-    #     package ([str]): Package name with version
+#     # Args:
+#     #     package ([str]): Package name with version
     
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-def install_local_package(folder_name):
-    # Install a local python package
+# def install_local_package(folder_name):
+#     # Install a local python package
 
-    # Args:
-    #     folder_name ([str]): name of the folder placed in evaluation_script/
+#     # Args:
+#     #     folder_name ([str]): name of the folder placed in evaluation_script/
     
-    subprocess.check_output(
-    [
-        sys.executable,
-        "-m",
-        "pip",
-        "install",
-        os.path.join(str(Path(__file__).parent.absolute()) + folder_name),
-    ]
-)
+#     subprocess.check_output(
+#     [
+#         sys.executable,
+#         "-m",
+#         "pip",
+#         "install",
+#         os.path.join(str(Path(__file__).parent.absolute()) + folder_name),
+#     ]
+# )
 
-install("modal==0.64.10")
+# install("modal==0.64.10")
 
 # install_local_package("package_folder_name")
 
