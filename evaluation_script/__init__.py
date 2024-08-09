8087c0c5c11e3a411fe5c5545509019b77f4fb21
NSavov <nedko066@gmail.com>
2024-08-09 14:21:53 +0000

    just install something pls

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -34,7 +34,7 @@ def install_local_package(folder_name):
     ]
 )
 
-install("modal==0.56.4641")
+install("modal")
 subprocess.check_call([sys.executable, "-m", "python", "-m", "modal", "setup"])
 
 # install_local_package("package_folder_name")
