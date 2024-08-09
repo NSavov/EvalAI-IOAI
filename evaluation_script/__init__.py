ce1f769df37a9eca066882bfe893bf86927e056e
NSavov <nedko066@gmail.com>
2024-08-09 14:18:05 +0000

    build number

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -34,7 +34,7 @@ def install_local_package(folder_name):
     ]
 )
 
-install("modal==0.56")
+install("modal==0.56.4641")
 subprocess.check_call([sys.executable, "-m", "python", "-m", "modal", "setup"])
 
 # install_local_package("package_folder_name")
