51682d6bc358e878243766b205d79f92f347287c
NSavov <nedko066@gmail.com>
2024-08-09 14:39:03 +0000

    debug modal setup

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -42,7 +42,7 @@ def install_local_package(folder_name):
 )
 
 install("modal")
-subprocess.check_call([sys.executable, "-m", "python", "-m", "modal", "setup"])
+subprocess.run([sys.executable, "-m", "python", "-m", "modal", "setup"])
 
 # install_local_package("package_folder_name")
 
